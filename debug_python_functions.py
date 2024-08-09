5c36aac47cea962121f4be63aeada858fc31203a
Ellie190 <nimyellie0512@gmail.com>
2024-08-09 04:40:42 +0200

    github workshop files

---
+++ debug_python_functions.py
@@ -0,0 +1,32 @@
+# Activity: Debug these functions
+
+# Function 1: Adding two numbers
+def add_numbers(a, b  
+    raturn a + b
+
+# Function 2: Getting the square of a number
+def square(num)
+    retnrn Num ** 2  
+
+
+# Function 3: Finding the maximum of two numbers
+def max_number(a, b):
+    if a > b: 
+        return a
+    elsi 
+        return c
+
+  
+# Function 4: Calculating the average of a list of numbers
+def average(numbers):
+    return sum(nuMbers) / len(numbers  
+
+
+# Function 5: Checking if a number is positive, negative, or zero
+def check_number(number):
+    if number > 0:
+        return "Positive"
+    else if number < 0:  
+        return "Negative"
+    else:
+        return "Zero"
---
+++ hello_git.py
@@ -0,0 +1,12 @@
+print("Hello everyone, welcome to the Git and GitHub workshop\n")
+
+print("Git is a distributed version control system that enables "
+      "multiple developers to track changes in source code, "
+      "collaborate efficiently, and manage project versions.\n")
+
+print("GitHub is a web-based platform that hosts Git repositories, "
+      "providing tools for version control, collaboration, and project management for developers.\n")
+
+add_numbers = 1 + 1
+
+print("1+1 is ", add_numbers, "not 11")
